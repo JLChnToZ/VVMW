ffc2cac32e83d0324724da4815ee12219eba2afb
JLChnToZ <jeremylam619@gmail.com>
2023-07-28 21:35:04 +0800

    Update validator 0.1.3

--- Packages/idv.jlchntoz.vvmw/Editor/Common/BindEventPreprocessor.cs
+++ Packages/idv.jlchntoz.vvmw/Editor/Common/BindEventPreprocessor.cs
@@ -31,6 +31,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
+using UnityEditor;
 using UnityEditor.Events;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
@@ -45,6 +46,7 @@ using UnityObject = UnityEngine.Object;
 namespace JLChnToZ.VRC.VVMW.Editors {
     internal sealed class BindEventPreprocessor : IProcessSceneWithReport {
         static readonly Regex regexCompositeFormat = new Regex(@"\{(\d+)[,:]?[^\}]*\}", RegexOptions.Compiled);
+        static readonly Dictionary<Type, MonoScript> scriptMap = new Dictionary<Type, MonoScript>();
         readonly Dictionary<Type, FieldInfo[]> filteredFields = new Dictionary<Type, FieldInfo[]>();
 
         public int callbackOrder => 0;
@@ -114,55 +116,80 @@ namespace JLChnToZ.VRC.VVMW.Editors {
 
         [DidReloadScripts]
         static void Validate() {
-            foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes()))
-            foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-            foreach (var attribute in field.GetCustomAttributes<BindEventAttribute>(true)) {
-                if (!type.IsSubclassOf(typeof(UdonSharpBehaviour))) {
-                    Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: Type is not derived from UdonSharpBehaviour, which will not have any effect.");
-                    continue;
-                }
-                var srcName = attribute.Source;
-                if (string.IsNullOrEmpty(srcName)) {
-                    Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: Source is empty.");
-                    continue;
-                }
-                var destName = attribute.Destination;
-                if (string.IsNullOrEmpty(destName)) {
-                    Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: Destination is empty.");
-                    continue;
-                }
-                var srcType = field.FieldType;
-                if (srcType.IsArray) srcType = srcType.GetElementType();
-                var srcProperty = srcType.GetProperty(srcName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-                if (srcProperty == null) {
-                    var srcField = srcType.GetField(srcName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-                    if (srcField == null) {
-                        Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: Source {srcType.FullName}.{srcName} does not exists.");
-                        continue;
-                    }
-                    srcType = srcField.FieldType;
-                } else
-                    srcType = srcProperty.PropertyType;
-                if (!srcType.IsSubclassOf(typeof(UnityEventBase))) {
-                    Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: Source {srcType.FullName}.{srcName} is not an Unity event. Only Unity events can be bound.");
-                    continue;
-                }
-                var regex = regexCompositeFormat.IsMatch(destName) ? new Regex(regexCompositeFormat.Replace(destName, ReplaceRegex), RegexOptions.Compiled) : null;
-                bool hasAnyMatch = false;
-                foreach (var member in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy))
-                    if (regex != null ? regex.IsMatch(member.Name) : destName == member.Name) {
-                        hasAnyMatch = true;
-                        if (member.GetParameters().Length > 0)
-                            Debug.LogWarning($"BindEventAttribute on {type.FullName}.{field.Name}: Destination {member.Name} has parameters, which still can be called, but will cause unexpected behavior.");
-                        if (!member.IsPublic)
-                            Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: Destination {member.Name} is not public, which cannot be called from outside.");
-                    }
-                if (!hasAnyMatch)
-                    Debug.LogError($"BindEventAttribute on {type.FullName}.{field.Name}: None of destination methods matches {destName}.");
-                else if (!destName.StartsWith("_"))
-                    Debug.LogWarning($"BindEventAttribute on {type.FullName}.{field.Name}: Destination {destName} does not start with underscore, which can be called over network RPC.\nIf this is intentional, you may safely ignore this warning.");
-            
+            GatherMonoScripts();
+            foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes())) {
+                foreach (var attribute in type.GetCustomAttributes<BindEventAttribute>(true)) ValidateType(type, attribute);
+                foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+                foreach (var attribute in field.GetCustomAttributes<BindEventAttribute>(true)) ValidateType(type, attribute, field);
             }
+        }
+
+        static void GatherMonoScripts() {
+            scriptMap.Clear();
+            foreach (var script in AssetDatabase.FindAssets("t:MonoScript").Select(AssetDatabase.GUIDToAssetPath).Select(AssetDatabase.LoadAssetAtPath<MonoScript>)) {
+                var type = script.GetClass();
+                if (type != null) scriptMap[type] = script;
+            }
+        }
+
+        static void ValidateType(Type type, BindEventAttribute attribute, FieldInfo field = null) {
+            var messagePrefix = field != null ? $"BindEventAttribute on {type.FullName}.{field.Name}: " : $"BindEventAttribute on {type.FullName}: ";
+            scriptMap.TryGetValue(type, out var currentScript);
+            if (!type.IsSubclassOf(typeof(UdonSharpBehaviour))) {
+                Debug.LogError($"{messagePrefix} Type is not derived from UdonSharpBehaviour, which will not have any effect.", currentScript);
+                return;
+            }
+            var srcName = attribute.Source;
+            if (string.IsNullOrEmpty(srcName)) {
+                Debug.LogError($"{messagePrefix} Source is empty.", currentScript);
+                return;
+            }
+            var destName = attribute.Destination;
+            if (string.IsNullOrEmpty(destName)) {
+                Debug.LogError($"{messagePrefix} Destination is empty.", currentScript);
+                return;
+            }
+            Type srcType;
+            if (field != null) {
+                srcType = field.FieldType;
+                if (attribute.SourceType != null) Debug.LogWarning($"{messagePrefix} SourceType is ignored because the attribute is on a field.", currentScript);
+            } else {
+                srcType = attribute.SourceType;
+                if (srcType == null) {
+                    Debug.LogError($"{messagePrefix} SourceType is empty.", currentScript);
+                    return;
+                }
+            }
+            if (srcType.IsArray) srcType = srcType.GetElementType();
+            var srcProperty = srcType.GetProperty(srcName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+            if (srcProperty == null) {
+                var srcField = srcType.GetField(srcName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+                if (srcField == null) {
+                    Debug.LogError($"{messagePrefix} Source {srcType.FullName}.{srcName} does not exists.", currentScript);
+                    return;
+                }
+                srcType = srcField.FieldType;
+            } else
+                srcType = srcProperty.PropertyType;
+            if (!srcType.IsSubclassOf(typeof(UnityEventBase))) {
+                Debug.LogError($"{messagePrefix} Source {srcType.FullName}.{srcName} is not an Unity event. Only Unity events can be bound.", currentScript);
+                return;
+            }
+            var regex = regexCompositeFormat.IsMatch(destName) ? new Regex(regexCompositeFormat.Replace(destName, ReplaceRegex), RegexOptions.Compiled) : null;
+            bool hasAnyMatch = false;
+            foreach (var member in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy))
+                if (regex != null ? regex.IsMatch(member.Name) : destName == member.Name) {
+                    hasAnyMatch = true;
+                    if (member.GetParameters().Length > 0)
+                        Debug.LogWarning($"{messagePrefix} Destination {member.Name} has parameters, which still can be called, but will cause unexpected behavior.", currentScript);
+                    if (!member.IsPublic)
+                        Debug.LogError($"{messagePrefix} Destination {member.Name} is not public, which cannot be called from outside.", currentScript);
+                }
+            if (!hasAnyMatch)
+                Debug.LogError($"{messagePrefix} None of destination methods matches {destName}.", currentScript);
+            else if (!destName.StartsWith("_"))
+                Debug.LogWarning($"{messagePrefix} Destination {destName} does not start with underscore, which can be called over network RPC.\nIf this is intentional, you may safely ignore this warning.", currentScript);
+
         }
 
         static string ReplaceRegex(Match match) {
