898c626509b78f268cefce70f518129d30cec30f
JLChnToZ <jeremylam619@gmail.com>
2024-09-08 17:02:04 +0800

    Reduce trigger area when video screen overlay UI is not active Fix some non-official package manager (e.g. VRC-Get/ALCOM) created projects unable to self update (Due to VPM Resolver not automatically included)

---
+++ Packages/idv.jlchntoz.vvmw/Runtime/VVMW/LaserTargetResizer.cs
@@ -0,0 +1,32 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace JLChnToZ.VRC.VVMW {
+    [RequireComponent(typeof(BoxCollider))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class LaserTargetResizer : UdonSharpBehaviour {
+        new BoxCollider collider;
+        [SerializeField] RectTransform inactiveRect, activeRect;
+        bool hasInit;
+
+        void Init() {
+            if (hasInit) return;
+            hasInit = true;
+            collider = GetComponent<BoxCollider>();
+        }
+
+        public void _OnActive() => SetRect(activeRect);
+
+        public void _OnInactive() => SetRect(inactiveRect);
+
+        void SetRect(RectTransform rectTransform) {
+            if (rectTransform == null) return;
+            Init();
+            var rect = rectTransform.rect;
+            collider.center = transform.InverseTransformPoint(rectTransform.TransformPoint(rect.center));
+            Vector3 size = rect.size;
+            size.z = 1;
+            collider.size = transform.InverseTransformVector(rectTransform.TransformVector(size));
+        }
+    }
+}
