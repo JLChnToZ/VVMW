923f7ff7cfc79acca8fbc74fd5691bc102100cff
JLChnToZ <jeremylam619@gmail.com>
2024-09-15 18:47:57 +0800

    Update editor locale definition

--- Packages/idv.jlchntoz.vvmw/Runtime/AssemblyInfo.cs
+++ Packages/idv.jlchntoz.vvmw/Runtime/AssemblyInfo.cs
@@ -1,2 +1,4 @@
 using System.Runtime.CompilerServices;
+using JLChnToZ.VRC.Foundation.I18N;
 [assembly: InternalsVisibleTo("JLChnToZ.VVMW.Editor")]
+[assembly: EditorI18NSource(LanguageAssetPath = "Packages/idv.jlchntoz.vvmw/Resources/editor-lang.json")]
