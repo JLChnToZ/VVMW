c25f072b990fd4c1d20707d8aebe1a64e0204768
JLChnToZ <jeremylam619@gmail.com>
2024-11-19 21:28:37 +0800

    Code tidyup

--- Packages/idv.jlchntoz.vvmw/Runtime/VVMW/Designer/LTCGIConfigurator.cs
+++ Packages/idv.jlchntoz.vvmw/Runtime/VVMW/Designer/LTCGIConfigurator.cs
@@ -9,22 +9,21 @@ namespace JLChnToZ.VRC.VVMW.Designer {
     [ExecuteInEditMode]
     [EditorOnly]
     internal class LTCGIConfigurator : MonoBehaviour, ISelfPreProcess {
+        internal static Action<LTCGIConfigurator, Core, LTCGI_Controller, List<LTCGI_Screen>> OnPreProcess;
         [HideInInspector] public Core core;
         [HideInInspector] public LTCGI_Controller controller;
         [HideInInspector] public List<LTCGI_Screen> screens;
-        internal static Action<LTCGIConfigurator, Core, LTCGI_Controller, List<LTCGI_Screen>> OnPreProcess;
 
         public int Priority => 0;
+
+        void ISelfPreProcess.PreProcess() =>
+            OnPreProcess?.Invoke(this, core, controller, screens);
 
         void OnDestroy() {
             if (Application.isPlaying) return;
             foreach (var screen in screens)
                 if (screen != null)
                     DestroyImmediate(screen);
-        }
-
-        public void PreProcess() {
-            OnPreProcess?.Invoke(this, core, controller, screens);
         }
     }
 }
