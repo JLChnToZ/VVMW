86c6c12c5e3f7e245caca4265ca5f106dee2a1bc
JLChnToZ <jeremylam619@gmail.com>
2025-12-31 16:42:30 +0800

    Enhance yt-dlp management for playlists and localization

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Enhanced YT-DLP version management and localization for playlist fetcher
+- Adjusted language selection UI
 
 ## 1.4.14 - 2025-12-25
 
--- Packages/idv.jlchntoz.vvmw/Editor/Common/YtdlpResolver.cs
+++ Packages/idv.jlchntoz.vvmw/Editor/Common/YtdlpResolver.cs
@@ -1,10 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading;
 using System.Diagnostics;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEditor;
 using Cysharp.Threading.Tasks;
+using JLChnToZ.VRC.Foundation.I18N;
+using JLChnToZ.VRC.Foundation.I18N.Editors;
 using JLChnToZ.VRC.Foundation.ThirdParties.LitJson;
 
 namespace JLChnToZ.VRC.VVMW.Editors {
@@ -19,6 +23,7 @@ namespace JLChnToZ.VRC.VVMW.Editors {
 #endif
         static string ytdlpPath;
         static bool hasYtdlp = false;
+        static bool hasCheckedYtdlp = false;
 
         static string YtdlpPath {
             get {
@@ -35,9 +40,13 @@ namespace JLChnToZ.VRC.VVMW.Editors {
         }
 
         public static UniTask DownLoadYtDlpIfNotExists() {
-            if (HasYtDlp()) return UniTask.CompletedTask;
-            if (!EditorUtility.DisplayDialog("Download yt-dlp", $"yt-dlp not found, do you want to download it from {YTDLP_DOWNLOAD_PATH}?", "Yes", "No"))
+            var hasYtdlp = HasYtDlp();
+            if (hasYtdlp && hasCheckedYtdlp) return UniTask.CompletedTask;
+            if (!EditorI18N.Instance.DisplayLocalizedDialog2("YTDLPResolver.download_confirm")) {
+                if (hasYtdlp) hasCheckedYtdlp = true;
                 return UniTask.CompletedTask;
+            }
+            hasCheckedYtdlp = true;
             return DownLoadYtDlp();
         }
 
@@ -47,9 +56,10 @@ namespace JLChnToZ.VRC.VVMW.Editors {
             var handler = new DownloadHandlerFile(path) { removeFileOnAbort = true };
             request.downloadHandler = handler;
             _ = request.SendWebRequest();
+            var i18n = EditorI18N.Instance;
             while (!request.isDone) {
                 await UniTask.Yield();
-                if (EditorUtility.DisplayCancelableProgressBar("Downloading", "Downloading yt-dlp", request.downloadProgress)) {
+                if (EditorUtility.DisplayCancelableProgressBar(i18n["YTDLPResolver.download_progress:title"], i18n["YTDLPResolver.download_progress:content"], request.downloadProgress)) {
                     request.Abort();
                     EditorUtility.ClearProgressBar();
                     return;
@@ -83,40 +93,38 @@ namespace JLChnToZ.VRC.VVMW.Editors {
         public static async UniTask<List<YtdlpPlayListEntry>> GetPlayLists(string url) {
             await DownLoadYtDlpIfNotExists();
             if (!HasYtDlp()) return new List<YtdlpPlayListEntry>();
-            List<YtdlpPlayListEntry> results = null;
-            try {
-                EditorUtility.DisplayProgressBar("Getting Playlists", "Getting Playlists", 0);
-                results = await Fetch(url);
-            } finally {
-                EditorUtility.ClearProgressBar();
-            }
-            return results;
+            var text = EditorI18N.Instance["YTDLPResolver.get_playlists"];
+            using var progress = new CancellableProgressBar(text, text);
+            return await Fetch(url, progress);
         }
 
         public static async UniTask FetchTitles(YtdlpPlayListEntry[] entries) {
             await DownLoadYtDlpIfNotExists();
             if (!HasYtDlp()) return;
-            try {
-                EditorUtility.DisplayProgressBar("Getting Titles", "Getting Titles", 0);
-                for (int i = 0; i < entries.Length; i++) {
-                    var entry = entries[i];
-                    if (!string.IsNullOrEmpty(entry.title) ||
-                        string.IsNullOrEmpty(entry.url)) continue;
-                    var results = await Fetch(entry.url);
-                    if (results.Count > 0) {
-                        entry.title = results[0].title;
-                        entry.url = results[0].url;
-                    }
-                    entries[i] = entry;
-                    EditorUtility.DisplayProgressBar("Getting Titles", $"Getting Titles ({i + 1}/{entries.Length})", (float)(i + 1) / entries.Length);
+            var text = EditorI18N.Instance["YTDLPResolver.get_titles"];
+            using var progress = new CancellableProgressBar(text, text);
+            var token = progress.CancelToken;
+            if (token.IsCancellationRequested) return;
+            for (int i = 0; i < entries.Length; i++) {
+                progress.Info = $"{text} ({i + 1}/{entries.Length})";
+                var entry = entries[i];
+                if (!string.IsNullOrEmpty(entry.title) ||
+                    string.IsNullOrEmpty(entry.url) ||
+                    token.IsCancellationRequested) continue;
+                var results = await Fetch(entry.url, progress, (float)i / entries.Length, (float)(i + 1) / entries.Length);
+                if (results.Count > 0) {
+                    entry.title = results[0].title;
+                    entry.url = results[0].url;
                 }
-            } finally {
-                EditorUtility.ClearProgressBar();
+                entries[i] = entry;
             }
         }
 
-        static async UniTask<List<YtdlpPlayListEntry>> Fetch(string url) {
+        static async UniTask<List<YtdlpPlayListEntry>> Fetch(string url, CancellableProgressBar progress, float startProgress = 0F, float endProgress = 1F) {
+            var cancelToken = progress.CancelToken;
+            var orgInfo = progress.Info;
             var results = new List<YtdlpPlayListEntry>();
+            if (cancelToken.IsCancellationRequested) return results;
             var startInfo = new ProcessStartInfo(YtdlpPath, $"--flat-playlist --no-write-playlist-metafiles --no-exec -sijo - {url}") {
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
@@ -127,13 +135,23 @@ namespace JLChnToZ.VRC.VVMW.Editors {
             var stderr = process.StandardError;
             while (!stderr.EndOfStream)
                 try {
-                    var line = await stderr.ReadLineAsync();
+                    cancelToken.ThrowIfCancellationRequested();
+                    var line = await stderr.ReadLineAsync().AsUniTask().AttachExternalCancellation(cancelToken);
                     if (!line.StartsWith("{")) continue;
                     var json = JsonMapper.ToObject(line);
                     results.Add(new YtdlpPlayListEntry {
                         title = json["title"].ToString(),
                         url = json.ContainsKey("url") ? json["url"].ToString() : url
                     });
+                    var index = json.ContainsKey("playlist_index") ? (int)json["playlist_index"] : 1;
+                    var count = json.ContainsKey("n_entries") ? (int)json["n_entries"] : 1;
+                    progress.Report(Mathf.Lerp(startProgress, endProgress, (float)index / count));
+                    progress.Info = $"{orgInfo} ({index}/{count})";
+                } catch (OperationCanceledException) {
+                    try {
+                        if (!process.HasExited) process.Kill();
+                    } catch { }
+                    break;
                 } catch { }
             await UniTask.SwitchToMainThread();
             return results;
@@ -143,5 +161,48 @@ namespace JLChnToZ.VRC.VVMW.Editors {
     public struct YtdlpPlayListEntry {
         public string title;
         public string url;
+    }
+
+    class CancellableProgressBar : IDisposable, IProgress<float> {
+        readonly CancellationTokenSource cts;
+        string title;
+        string info;
+        float progress;
+
+        public string Title {
+            get => title;
+            set {
+                title = value;
+                Report(progress);
+            }
+        }
+
+        public string Info {
+            get => info;
+            set {
+                info = value;
+                Report(progress);
+            }
+        }
+
+        public CancellationToken CancelToken => cts.Token;
+
+        public CancellableProgressBar(string title, string info, float initialProgress = 0) {
+            cts = new CancellationTokenSource();
+            this.title = title;
+            this.info = info;
+            Report(initialProgress);
+        }
+
+        public void Report(float value) {
+            progress = value;
+            if (EditorUtility.DisplayCancelableProgressBar(title, info, value))
+                cts.Cancel();
+        }
+
+        public void Dispose() {
+            EditorUtility.ClearProgressBar();
+            cts.Dispose();
+        }
     }
 }
